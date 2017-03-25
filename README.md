3bbacdb62518e17b22799c5ffa3566c03ae02738
AvivAbramovich <AvivAbramovich@gmail.com>
2017-03-25 17:11:45 +0300

    init project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # PythonProjectUnifier
+
+This script should unify a whole python project into a single python source file, so it would be easier for you to run it everywhere without carry about dependencies and relative files and imports
+This script relocate all the in a single python file, under classes, class for each module and python directory, and fix the import between them, if needed
+
+# TODO
+1. add to the output script a main, so it could run an individual module or script from the original project
+2. fix imports and 'as' statements in them
--- main.py
+++ main.py
@@ -1,7 +1,50 @@
 from sys import argv, exit
-from os.path import isdir
-from parse_project_structure import parse_project_structure
-from append_module import enumerate_modules_structure
+from os.path import isdir, join
+from os import walk
+
+__author__ = 'AvivAbramovich'
+__version__ = '0.0.1'
+
+def path_to_dirs_list(orig_path, current_path):
+    relative_path = current_path[len(orig_path)+1:]
+    return relative_path.split('/') # for unix based
+
+def parse_project_structure(base_path):
+    modules_structure = {}
+    for current_path, sub_dirs, files in walk(base_path):
+        dirs_list = path_to_dirs_list(base_path, current_path)
+        pointer = modules_structure
+        for dir in dirs_list:
+            if len(dir) > 0:
+                if dir not in pointer:
+                    pointer[dir] = {'type': 'directory'}
+                pointer = pointer[dir]
+        for file in files:
+            if file.endswith('.py'):
+                pointer[file] = {'type': 'module', 'path': join(current_path, file), 'name' : file}
+    return modules_structure
+
+def append_module(output_file_handler, depth, module_path, module_name):
+    ident = ''
+    for i in xrange(0, depth):
+        ident += '\t'
+    output_file_handler.write(ident + 'class ' + module_name[:-3] + ':\r\n') # -3 remove the .py
+    num_lines = 0
+    with open(module_path, 'r') as in_fh:
+        for line in in_fh:
+            num_lines += 1
+            output_file_handler.write(ident + '\t' + line + '\r\n')
+    if num_lines == 0:  # empty file
+        output_file_handler.write(ident + '\tpass\r\n')
+
+
+def enumerate_modules_structure(modules_structure, output_file_handler, depth=0):
+    for key in modules_structure.keys():
+        if type(modules_structure[key]) is dict:
+            if modules_structure[key]['type'] == 'module':
+                append_module(output_file_handler, depth, modules_structure[key]['path'], modules_structure[key]['name'])
+            else:
+                enumerate_modules_structure(modules_structure[key], output_file_handler, depth+1)
 
 if __name__ == '__main__':
     if len(argv) != 3:
@@ -16,4 +59,5 @@ if __name__ == '__main__':
 
     modules_structure = parse_project_structure(base_path)
     with open(output_path, 'w') as o_fh:
+        o_fh.write('Unified by Python-Projects-Unifier by {}, version {}\r\n'.format(__author__, __version__))
         enumerate_modules_structure(modules_structure, o_fh)
